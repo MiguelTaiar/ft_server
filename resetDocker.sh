47a0b7096a7b0f9dcb667ac6ee249e9ff1eea0d9
Iroh <marvin@42.fr>
2020-04-09 10:15:53 -0300

    Deleted docker commands from ft_server/

--- resetDocker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker rm -f ft_container
-docker rmi ft_server
--- runDocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t ft_server .
-docker run -itd -p 80:80 -p 443:443 --name ft_container ft_server
-docker ps -a
